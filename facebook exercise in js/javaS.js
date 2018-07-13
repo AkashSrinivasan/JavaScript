e561f13d256ef1e3aaac5184d0843398b1d8003f
Akash <37179506+AkashSrinivasan@users.noreply.github.com>
2018-07-13 15:01:24 +0530

    Create javaS.js

---
+++ facebook exercise in js/javaS.js
@@ -0,0 +1,54 @@
+
+
+
+
+var dataBase = [
+		{
+			username : "akash",
+			password : "12345"
+		},
+		{
+			username : "mohan",
+			password : "55555"
+		},
+		{
+			username : "harish",
+			password : "66666"
+		},
+];
+var newsFeed = [
+		{
+			username : "uncle",
+			news : "all are worst in everything"
+
+		},
+		{
+			username : "daddy",
+			news : "i love everything"
+		}
+];
+
+var userNamePrompt = prompt("enter ur user name");
+var passwordPrompt = prompt("enter ur password");
+function isValid(username,password)
+{
+	for(var i=0 ; i < dataBase.length ; i++)
+	{
+		if(username === dataBase[i].username && password === dataBase[i].password){
+		return true;
+	}
+	}
+	return false;
+}
+
+
+function signIN(user ,pass)
+{
+	if(isValid(user,pass)){
+		console.log(newsFeed);
+	}else
+	{
+		alert("oops...!! sorry wrong username and password");
+	}
+}
+signIN(userNamePrompt,passwordPrompt)
